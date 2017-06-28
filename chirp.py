8e66fe972ee6738a5d8a8156c6307e5877794bfa
David Owen <david.owen.14@ucl.ac.uk>
2017-06-28 17:38:00 +0100

    Change tweet length-handling to work with t.co url shortening.

--- chirp.py
+++ chirp.py
@@ -11,13 +11,16 @@ def main():
                       access_token_secret=keys.access_token_secret)
 
     tweet = ' '.join(sys.argv[1:])
-    if len(tweet) > 0 and len(tweet) <= 140:
-        api.PostUpdate(tweet)
+    try:
+        status = api.PostUpdate(tweet)
         print('Posted: ' + tweet)
-    elif len(tweet) == 0:
-        print('No tweet found to post.')
-    elif len(tweet) > 140:
-        print('Tweet too long.')
+    except twitter.error.TwitterError:
+        if len(tweet) == 0:
+            print('No tweet found to post.')
+        elif len(tweet) > 0 and len(tweet) < 140:
+            print("Unexpected error posting. Invalid character?")
+        elif len(tweet) > 140:
+            print('Tweet too long.')
 
 if __name__ == "__main__":
     main()
